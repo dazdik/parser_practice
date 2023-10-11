b3c450f611becef92fe7e6be627e3ff7e17ca1cb
Дарья Коннова <dashustic@yandex.ru>
2023-10-11 19:36:04 +0300

    add some tasks

---
+++ parser.py
@@ -0,0 +1,26 @@
+import lxml
+from bs4 import BeautifulSoup
+
+import requests
+
+
+url = 'https://parsinger.ru/4.3/5/index.html'
+
+response = requests.get(url=url)
+
+response.encoding = 'utf-8'
+
+
+def calculate_total_price(html: str) -> float:
+    # Инициализация BeautifulSoup.
+    soup = BeautifulSoup(html, 'lxml')
+    price_for_all = soup.find_all('p', class_='count price')
+    total_count = soup.find_all('p', class_='count stock')
+    total_price = 0
+    for price, count in zip(price_for_all, total_count):
+        total_price += (float(price.text.split('$')[1]) * int(count.text.split(': ')[1]))
+
+    return total_price
+
+
+print(f"Общая стоимость в случае продажи всех товаров: ${calculate_total_price(response.text)}")
---
+++ parser_username.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+import requests
+
+
+response = requests.get('https://parsinger.ru/3.4/3/dialog.json').json()
+
+list_usernames = []
+
+
+def count_users(resp):
+
+    list_usernames.append(resp['username'])
+
+    if resp['comments']:
+        for i in resp['comments']:
+            count_users(i)
+    return list_usernames
+
+
+not_sorted_names = Counter(count_users(response))
+sorted_names = dict(sorted(not_sorted_names.items(), key=lambda x: (-x[1], x[0])))
+print(sorted_names)
+
+
+
+
+
+
