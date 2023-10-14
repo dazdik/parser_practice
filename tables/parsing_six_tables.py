bf1a8b75ea9397c93b6428af343bda2bda85da26
Дарья Коннова <dashustic@yandex.ru>
2023-10-14 16:02:23 +0300

    add parsing_six_tables

---
+++ tables/parsing_six_tables.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://parsinger.ru/4.8/7/index.html'
+response = requests.get(url=url)
+response.encoding = 'utf-8'
+
+soup = BeautifulSoup(response.text, 'lxml')
+
+data = soup.select('td')
+print(sum(int(d.text) for d in data if int(d.text) % 3 == 0))
