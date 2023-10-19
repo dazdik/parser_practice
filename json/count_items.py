35f7f55ad95b4c4c0f0508350e9f3d5dac53f0da
Дарья Коннова <dashustic@yandex.ru>
2023-10-19 17:48:05 +0300

    added count_items

---
+++ json/count_items.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+import requests
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+headers = {'User-Agent': ua.random}
+url = 'https://parsinger.ru/downloads/get_json/res.json'
+response = requests.get(url=url, headers=headers).json()
+
+res = defaultdict(int)
+
+for item in response:
+    name_category = item.get('categories')
+    res[name_category] += int(item['count'])
+
+print(dict(res))
