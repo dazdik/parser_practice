2a929500000918fd6b169bcb36c842813a9a26c2
Дарья Коннова <dashustic@yandex.ru>
2023-10-21 01:45:18 +0300

    update fill_form

--- selenium/fill_form.py
+++ selenium/fill_form.py
@@ -3,27 +3,35 @@ import time
 
 from dotenv import load_dotenv
 from fake_useragent import UserAgent
+from faker import Faker
+from seleniumwire import webdriver
 
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 load_dotenv()
 
-options = {'proxy': {
+seleniumwire_options = {'proxy': {
     'http': os.getenv('HTTP_PROXY'),
     'https': os.getenv('HTTPS_PROXY'),
 }}
 
 ua = UserAgent()
-opts = webdriver.ChromeOptions()
-opts.add_argument(f"user-agent={ua.random}")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument(f"user-agent={ua.random}")
 
-with webdriver.Chrome(seleniumwire_options=options, options=opts) as browser:
+fake = Faker('ru_RU')
+fake.random
+info = [fake.first_name_male(), fake.last_name_male(),
+        fake.middle_name_male(), fake.random_int(10, 99),
+        fake.city_name(), fake.email()
+        ]
+
+with webdriver.Chrome(options=chrome_options) as browser:
     browser.get('https://parsinger.ru/selenium/1/1.html')
-    fields = browser.find_elements(By.CLASS_NAME, 'form_box')
+    fields = browser.find_elements(By.CLASS_NAME, 'form')
 
-    for field in fields:
-        form_element = field.find_element(By.CLASS_NAME, 'form').send_keys('Text')
+    for form_text, field in zip(info, fields):
+        field.send_keys(form_text)
 
     send_element = browser.find_element(By.CLASS_NAME, 'btn').click()
 
