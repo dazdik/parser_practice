0a3d8548e832d3004ac198ded6d7f68a431c105e
Дарья Коннова <dashustic@yandex.ru>
2023-10-22 17:23:57 +0300

    added new tasks

--- selenium/check_box_2.py
+++ selenium/check_box_2.py
@@ -1,3 +1,5 @@
+from numbers import numbers
+
 from fake_useragent import UserAgent
 
 from selenium import webdriver
@@ -7,10 +9,12 @@ ua = UserAgent()
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(f'user_agent={ua.random}')
 
+
 with webdriver.Chrome(options=chrome_options) as browser:
-    browser.get('https://parsinger.ru/selenium/4/4.html')
+    browser.get('https://parsinger.ru/selenium/5/5.html')
     checkbox = browser.find_elements(By.CLASS_NAME, 'check')
     for ch in checkbox:
-        ch.click()
+        if int(ch.get_attribute('value')) in numbers:
+            ch.click()
     browser.find_element(By.CLASS_NAME, 'btn').click()
     print(browser.find_element(By.ID, 'result').text)
--- selenium/sum_of_opton.py
+++ selenium/sum_of_opton.py
@@ -1,3 +1,5 @@
+import time
+
 from fake_useragent import UserAgent
 
 from selenium import webdriver
@@ -8,9 +10,10 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument(f'user_agent={ua.random}')
 
 with webdriver.Chrome(options=chrome_options) as browser:
-    browser.get('https://parsinger.ru/selenium/4/4.html')
-    checkbox = browser.find_elements(By.CLASS_NAME, 'check')
-    for ch in checkbox:
-        ch.click()
+    browser.get('https://parsinger.ru/selenium/7/7.html')
+    all_options = browser.find_elements(By.TAG_NAME, 'option')
+    value_form = sum(int(opt.text) for opt in all_options)
+    browser.find_element(By.ID, 'input_result').send_keys(value_form)
+
     browser.find_element(By.CLASS_NAME, 'btn').click()
-    print(browser.find_element(By.ID, 'result').text)
+    time.sleep(5)
