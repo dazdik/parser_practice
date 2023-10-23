3c4bb3e8920280662573a4d454df24f9d22693c8
Дарья Коннова <dashustic@yandex.ru>
2023-10-23 17:37:13 +0300

    update clear_fields

--- selenium/clear_fields.py
+++ selenium/clear_fields.py
@@ -5,10 +5,11 @@ from selenium.webdriver.common.by import By
 chrome_options = webdriver.ChromeOptions()
 
 with webdriver.Chrome(options=chrome_options) as browser:
-    browser.get('https://parsinger.ru/selenium/5.5/1/1.html')
+    browser.get('https://parsinger.ru/selenium/5.5/2/1.html')
     fields = browser.find_elements(By.CLASS_NAME, 'text-field')
     for field in fields:
-        field.clear()
+        if field.get_attribute('data-enabled'):
+            field.clear()
 
     browser.find_element(By.ID, 'checkButton').click()
     alert = browser.switch_to.alert.text
