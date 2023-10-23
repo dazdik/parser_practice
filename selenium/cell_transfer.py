68c529ab1c4eeaefebba7287afc7edbc62f83605
Дарья Коннова <dashustic@yandex.ru>
2023-10-24 00:49:17 +0300

    add tasks

---
+++ selenium/cell_transfer.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+
+browser.get("https://parsinger.ru/selenium/5.5/4/1.html")
+time.sleep(2)
+
+
+# Находим все родительские элементы
+parents = browser.find_elements(By.CLASS_NAME, 'parent')
+
+# Проходимся по каждому родительскому элементу
+for parent in parents:
+    # Находим textarea и checkbox внутри родительского элемента
+    color = parent.find_elements(By.TAG_NAME, 'textarea')
+    number = color[0].text
+    color[0].clear()
+    color[1].send_keys(number)
+    parent.find_element(By.TAG_NAME, 'button').click()
+    time.sleep(2)
+browser.quit()
--- selenium/clear_fields.py
+++ selenium/clear_fields.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 chrome_options = webdriver.ChromeOptions()
 
 with webdriver.Chrome(options=chrome_options) as browser:
